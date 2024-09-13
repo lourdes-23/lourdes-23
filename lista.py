da8c8ab4797187b75efce3946a145d27c5f88503
lourdes-23 <maria.de.lourdes9230@gmail.com>
2024-09-13 15:55:41 -0300

    Create lista.py

---
+++ lista.py
@@ -0,0 +1,10 @@
+nome_completo = input("digite seu nome completo: ")
+
+
+nome = "maria de lourdes da silva santos"
+primeiro_nome = "maria"
+ultimo_nome = "santos"
+
+
+print(f"nome completo: {nome_completo.upper()}")
+print(f"Sobrenome: {ultimo_nome.lower()}")
